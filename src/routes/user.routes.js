6cbe7e570b4226ce5521f0f82a21395749e77114
Ian Restrepo <ianyrestrepo@gmail.com>
2023-10-25 00:06:22 -0500

    Finished CRUD API With MongoDB

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,44 @@ Router.post('/users', (req, res) => {
     .save()
     .then((data)=> res.send(data))
     .catch((error)=> res.json({message: error}))
+    
+});
 
+Router.get('/users', (req, res) => {
+    userSchema  // Promesa
+    .find() // encuentra los valores
+    .then((data)=> res.send(data))
+    .catch((error)=> res.json({ErrorMessage: error}))
+    
+});
+
+Router.get('/users/:id', (req, res) => {
+    const { id } = req.params; // Encuentra el id que se pone en la petición
+    userSchema
+    .findById(id) // encuentra el valor del id
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    
+});
+
+Router.put('/users/:id', (req, res) => {
+    const { id } = req.params; // Encuentra el id que se pone en la petición
+    const { userName, password, isVerified } = req.body;
+    userSchema
+    .updateOne({_id: id},{$set: {userName, password, isVerified}}) // encuentra el valor del id en el primer objeto y en el segundo se hacen los cambios con el objeto $set
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    
+});
+
+Router.delete('/users/:id', (req, res) => {
+    const { id } = req.params; // Encuentra el id que se pone en la petición
+    
+    userSchema
+    .deleteOne({ _id: id }) // encuentra el valor del id
+    .then((data)=> res.json(data))
+    .catch((error)=> res.json({message: error}))
+    
 });
 
 module.exports = Router;
